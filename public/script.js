05332dc61c4a1d336d10a8fb23f3956a9091078e
ebarza <fhb@mail.bg>
2024-09-03 23:42:40 +0300

    Use bootstrap-static to fetch all player names

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ function fetchPlayersData() {
     fetch('https://fantasy.premierleague.com/api/bootstrap-static/')
         .then(response => response.json())
         .then(data => {
+            // Populate the playersData object with player ID as the key and player name as the value
             playersData = data.elements.reduce((acc, player) => {
                 acc[player.id] = player.web_name;
                 return acc;
@@ -75,6 +76,7 @@ fetchAndDisplayStandings();
 
 // Fetch the player data when the page loads
 fetchPlayersData();
+
 
 
 
